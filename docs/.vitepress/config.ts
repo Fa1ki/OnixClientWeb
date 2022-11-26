f2cc42a8601755beff5939e6b95c4b439fdf45db
Jeremias <jeremias4905@gmail.com>
2022-11-26 16:03:33 +0100

    first commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,82 @@
+export default {
+    title: 'Onix Client',
+    themeConfig: {
+        logo: '/Logo.png',
+        // siteTitle: false,
+        footer: {
+            message: '',
+            copyright: 'Copyright © 2022 Onix Client'
+        },
+        localeLinks: {
+            text: 'English',
+            items: [
+              { text: 'German', link: '/' }
+            ]
+        },
+        socialLinks: [
+            { icon: 'discord', link: 'https://discord.gg/onixclient' }
+        ],
+        nav: [
+            { text: 'Patreon', link: 'https://www.patreon.com/onixclient' },
+            { text: 'FAQ', link: '/faq/', activeMatch: '/faq/' },
+            { text: 'Scripting', link: '/scripting/', activeMatch: '/scripting/' },
+            { text: 'About', link: '/about/', activeMatch: '/about/'},
+        ],
+        sidebar: [
+            {
+                text: 'Libaries',
+                items: [
+                    { text: 'client.setting', link: '/' },
+                    { text: 'player', link: '/' },
+                    { text: 'network', link: '/' },
+                    { text: 'event', link: '/' },
+                    { text: 'client', link: '/' },
+                    { text: 'gui', link: '/' },
+                    { text: 'fs', link: '/' },
+                    { text: 'gfx', link: '/' },
+                    { text: 'server', link: '/' },
+                    { text: 'gfx2', link: '/' },
+                    { text: 'dimension', link: '/' }
+                ],
+            },
+            {
+                text: 'Classes',
+                items: [
+                    { text: 'Font', link: '/' },
+                    { text: 'iColor', link: '/' },
+                    { text: 'Inventory', link: '/' },
+                    { text: 'Item', link: '/' },
+                    { text: 'Effect', link: '/' },
+                    { text: 'AttributeListHolder', link: '/' },
+                    { text: 'SelectedEntetyInfo', link: '/' },
+                    { text: 'Biome', link: '/' },
+                    { text: 'Theme', link: '/' },
+                    { text: 'Waypoint', link: '/' },
+                    { text: 'InventoryAmor', link: '/' },
+                    { text: 'BinaryFile', link: '/' },
+                    { text: 'Gfx2Texture', link: '/' },
+                    { text: 'Attribute', link: '/' },
+                    { text: 'Skin', link: '/' },
+                    { text: 'ScriptingModule', link: '/' },
+                    { text: 'RaycastInfo', link: '/' },
+                    { text: 'Waypoints', link: '/' },
+                    { text: 'Vector2', link: '/' },
+                    { text: 'ColorSetting', link: '/' },
+                    { text: 'VisualModule', link: '/' },
+                    { text: 'Setting', link: '/' },
+                    { text: 'Module', link: '/' },
+                    { text: 'Enchants', link: '/' },
+                    { text: 'LightPair', link: '/' },
+                    { text: 'Stats', link: '/' },
+                    { text: 'Block', link: '/' }
+                ],
+            },
+            {
+                text: 'Globals',
+                items: [
+                    { text: 'Globals', link: '/' }
+                ]
+            }
+        ]
+    }
+}
---
+++ docs/about/index.md
@@ -0,0 +1,24 @@
+---
+layout: home
+
+hero:
+    name: About
+    tagline: Lorem ipsum...
+
+
+features:
+  - icon: ⚡️
+    title: thunder
+    details: Lorem ipsum...
+    link: 'https://discord.gg/onixclient'
+  - icon: 🖖
+    title: hand
+    details: Lorem ipsum...
+    link: 'https://discord.gg/onixclient'
+  - icon: 🛠️
+    title: wow
+    details: Lorem ipsum...
+    link: 'https://discord.gg/onixclient'
+
+---
+
